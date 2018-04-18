ddf97e738003a0a39e508e79e61d68678b36cfa6
John Bowden <j.j.b1980@live.com>
2018-04-18 19:43:07 +1000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 
+import Anchor from 'grommet/components/ANchor'
 import Box from 'grommet/components/Box'
+import Button from 'grommet/components/Button'
 import GrrommetFooter from 'grommet/components/Footer'
 import Paragraph from 'grommet/components/Paragraph'
 import Title from 'grommet/components/Title'
 
+import SocialMailIcon from 'grommet/components/icons/base/SocialMail'
+import SocialTwitterIcon from 'grommet/components/icons/base/SocialTwitter'
+import SocialFacebookIcon from 'grommet/components/icons/base/SocialFacebook'
+
 import './Footer.scss'
 
 export default function Footer () {
-  return <GrrommetFooter full align='center' justify='between' margin={{top: 'large'}} primary size='large' className='footer'>
+  return <GrrommetFooter justify='center' margin={{top: 'large'}} primary size='large' className='footer'>
     <Box direction='row'
-      full
+      size='large'
       pad={{between: 'medium'}}
-      margin={{left: 'large'}}
+      margin={{left: 'medium', right: 'medium'}}
+      responsive={false}
     >
-      <Paragraph margin='none' className='footer-copyright'>
+      <Box size='large' full='horizontal' pad='small' flex >
         © 2018 Bowden Technology
-      </Paragraph>
+      </Box>
+      <Box
+        className='footer-support'
+        direction='row'
+        flex
+        justify='end'
+      >
+        <Button icon={<SocialMailIcon />} size='small' href='mailto:a@b.c'/>
+        <Button icon={<SocialTwitterIcon />} href='https://twitter.com'/>
+        <Button icon={<SocialFacebookIcon />} href='https://facebook.com'/>
+      </Box>
     </Box>
   </GrrommetFooter>
 }
